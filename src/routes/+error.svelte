3ea1b80a1a4597f43cd78d2505f27b508c7328ae
Lucien Neuhoff <scaffus.Scaffus@gmaiL.com>
2023-02-16 15:39:45 +0100

    Error message is now visible when in dark mode

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex h-full w-full">
-	<h1 class=" w-full self-center text-center text-xl text-zinc-800">
+	<h1 class=" w-full self-center text-center text-xl text-zinc-800 dark:text-zinc-300">
 		{$page.error?.message}
 	</h1>
 </div>
